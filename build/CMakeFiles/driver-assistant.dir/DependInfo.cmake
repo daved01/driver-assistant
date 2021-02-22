20df9dd55d0413323b16bf284079b27e2058710d
David <david.kirchhoff@icloud.com>
2021-02-22 19:31:01 +0100

    Added basic functionality

---
+++ build/CMakeFiles/driver-assistant.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/David/Repositories/driver-assistant/main.cpp" "/Users/David/Repositories/driver-assistant/build/CMakeFiles/driver-assistant.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/David/opencv/install/include/opencv4"
+  "/Users/David/libtorch/include"
+  "/Users/David/libtorch/include/torch/csrc/api/include"
+  "/usr/local/Cellar/python@3.9/3.9.1_4/Frameworks/Python.framework/Versions/3.9/include/python3.9"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
